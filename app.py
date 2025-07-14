3f87e43c9f88a19033b1bb440c8ab3a4e3b57ba8
stocktostockbot <stocktostock1000@gmail.com>
2025-07-14 23:49:05 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+from ai_signals import generate_signal
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    signal = generate_signal()
+    return render_template("index.html", signal=signal)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=10000)
