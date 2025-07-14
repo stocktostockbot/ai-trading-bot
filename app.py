7a48f6e51c0f87abbaaa245071dd62f0a800936d
stocktostockbot <stocktostock1000@gmail.com>
2025-07-15 00:10:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     signal = generate_signal()
-    return render_template("index.html", signal=signal)
+    return render_template('index.html', signal=signal)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=10000)
